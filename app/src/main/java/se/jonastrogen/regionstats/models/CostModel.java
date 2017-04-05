bcb171daca5da13fb82eda96f693290dfa12a8e9
Jonas Ohlund <jonas.ohlund@gmail.com>
2017-04-05 19:10:40 +0200

    Some models and launcher icons as well as a splashscreen.

---
+++ app/src/main/java/se/jonastrogen/regionstats/models/CostModel.java
@@ -0,0 +1,9 @@
+package se.jonastrogen.regionstats.models;
+
+class CostModel {
+    public String country;
+    public double small;
+    public double medium;
+    public double large;
+    public short emissions;
+}
---
+++ app/src/main/java/se/jonastrogen/regionstats/models/StatisticsModel.java
@@ -0,0 +1,9 @@
+package se.jonastrogen.regionstats.models;
+
+import java.util.List;
+
+class StatisticsModel {
+    public short revision;
+    public short hours;
+    public List<CostModel> cost;
+}
